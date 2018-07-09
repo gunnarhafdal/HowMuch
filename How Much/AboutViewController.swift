6034ee112e016a6103329651c19ca0c0a3a5bbd1
Gunnar Hafdal <gunnar.hafdal@valtech.com>
2018-07-09 07:23:38 +0200

    removed unused iboutlets in about controller

--- How Much/AboutViewController.swift
+++ How Much/AboutViewController.swift
@@ -10,41 +10,13 @@ import UIKit
 
 class AboutViewController: UIViewController {
 
-    @IBOutlet weak var fromButton: UIButton!
-    @IBOutlet weak var toButton: UIButton!
     @IBOutlet weak var goBackButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        let defaults = UserDefaults.standard
-        if let fromCurrency = defaults.string(forKey: defaultsKeys.fromCurrency) {
-            fromButton.setTitle("From \(fromCurrency)", for: .normal)
-        }
-        
-        if let toCurrency = defaults.string(forKey: defaultsKeys.toCurrency) {
-            toButton.setTitle("To \(toCurrency)", for: .normal)
-        }
-        Valuta().update()
-    }
-    
     @IBAction func goBack(_ sender: UIButton) {
         self.dismiss(animated: true)
     }
-    
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-        if let vc = segue.destination as? CurrencyTableViewController {
-            vc.currencyToChange = segue.identifier == "chooseFrom" ? defaultsKeys.fromCurrency : defaultsKeys.toCurrency
-        }
-     }
-    
 }
